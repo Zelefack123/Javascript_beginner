f4dbd3d350dbdbe18d2d248e4b94bb41a5291bd7
Zelefack123 <zelefackmarie858@gmail.com>
2024-11-26 16:31:34 +0100

    did the QR code project

--- weatherApp project/app.js
+++ weatherApp project/app.js
@@ -1,85 +1,87 @@
-const inputBox = document.getElementById("city");
-const temp = document.getElementById("humidity");
-const wind = document.getElementById("wind");
-const city = document.getElementById("temptown");
+const Input = document.querySelector("input");
+const Temperature = document.querySelector("#temp");
+const Humidity = document.querySelector("#humidity-value");
+const windspeed = document.querySelector("#windspeed-value");
+const town = document.querySelector("#temptown");
+const Image = document.querySelector("img");
+
+const cities = [
+ {
+ town: "Berlin",
+ humidity: 73,
+ Temperature: 25,
+ windspeed:10
+
+  },
+  {
+    town: "Douala",
+    humidity: 73,
+    Temperature: -4
+   
+     },
+     {
+        town: "Yaounde",
+        humidity: 25,
+        Temperature: 10,
+        windspeed: 10
+       
+     },
+     {
+        town: "Buea",
+        humidity: 10,
+        Temperature: 70,
+        windspeed:30,
+       
+     },
+     {
+        town: "Douala",
+        humidity: 20,
+        Temperature: 15,
+        windspeed:10,
+     },
 
 
-const towns=  [
-    {
-    town:"berlin",
-    Attributes:
-        {
-     wind: 7,
-     temperture: 8,
-     humidity: 10,
-
-        }
-    },
-    {
-
-    town:"Johannesburg",
-    Attributes:
-        {
-     wind: 1,
-     temperture: 8,
-     humidity:7
-
-        }
-    }
-    ,
-{
-    town:"Johannesburg",
-    Attributes:
-        {
-     wind: 3,
-     temperture: 9,
-     humidity:5
-
-        }
-    }
-    ,
-{
-    town:"Yaounde",
-    Attributes:
-        {
-     wind: 9,
-     temperture: 8,
-     humidity:10
-
-        }
-}
+]
 
 
 
-
-    ]
-
+let found;
 
 function displayCity(){
-     let inputCity =  inputBox.value;        
-    
-     const foundTown = towns.find(town => town.town === inputCity);
-     if(foundTown){
-    foundTown = city.innerHTML;
-    attributes.wind = wind.innerHTML;
-    attributes.humidity = inputBox.innerHTML;
-    attributes.temperature = inputBox.innerHTML;
-  }
-
-    else{
-        alert("the town you've selected doesn't exist")
+   
+    if (Input.value.trim === ""){
+        alert("Enter a town")
     }
-  }
+    else {
+    const towns = Input.value;
+    for (i = 0; i < cities.length; i++){
+        if(cities[i].town.toLowerCase() === towns.toLowerCase()){
+       Temperature.innerHTML = `${cities[i].Temperature}<sup>o</sup>c`
+       Humidity.innerHTML = `${cities[i].humidity}%`
+       windspeed.innerHTML =`${ cities[i].windspeed}Km/hr`;
+       town.innerHTML = cities[i].town;
+       // Update the weather image
+       if (cities[i].Temperature <= 0) {
+        Image.setAttribute(
+          "src",
+          "./images/snowfall_4731783-removebg-preview.png"
+        );
+      } else if (cities[i].Temperature > 0 && cities[i].Temperature <= 15) {
+        Image.setAttribute("src", "./images/rain_11844406-removebg-preview.png");
+      } else if (cities[i].Temperature > 15 && cities[i].Temperature <= 30) {
+        Image.setAttribute("src", "./images/cloudy..png");
+      } else {
+        Image.setAttribute("src", "./images/sun.png");
+      }
+
+    found = true;
+        } }
+ if(!found){
+    alert("the town does not exist")
+ }
+    
+    
 
 
-
-
-
-
-
-
-
-
-
-
-
+}
+}
